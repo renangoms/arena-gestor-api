c9dbc192545b54e002c9d6b81d7ff92f1325ed24
Renan Gomes <ti.renangomes@gmail.com>
2025-01-13 13:38:04 -0400

    feat: add validation for required environment variables

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,4 +1,23 @@
-export const env = {
-  jwtSecret: process.env.JWT_SECRET ?? 'secret',
-  frontEndURL: process.env.frontEndURL ?? '',
-};
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+dotenv.config();
+
+const envSchema = z.object({
+  dbURL: z.string().min(1),
+  jwtSecret: z.string().min(1),
+  frontendUrl: z
+    .string()
+    .refine(
+      (url) => url.startsWith('http') || url.startsWith('https'),
+      'Invalid URL format',
+    ),
+});
+
+type Env = z.infer<typeof envSchema>;
+
+export const env: Env = envSchema.parse({
+  dbURL: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+  frontendUrl: process.env.FRONT_URL,
+});
